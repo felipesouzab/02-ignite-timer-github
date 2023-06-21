c430814f46e4d94ebd5fc975e70d62eb70785772
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-06-21 19:06:31 -0300

    deploy configurations

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { CyclesContextProvider } from './contexts/CyclesContext'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
